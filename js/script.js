391edf442a0f7c181a948855c47ae11b7d8a6b82
Etec Prof. Horácio Augusto da Silveira <you@example.com>
2024-11-18 22:29:56 -0300

    aula17

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function mostraAlerta() {
+    alert("UAU");
+}
+
+function mostraAlerta1(msg) {
+    alert("Boa noite, "+msg);
+}
+
+function  mensagem_com_parametro_return() {
+    return("Você digitou" +msg1)
+}
+
+function somaF(n1, n2) {
+    n1=parseFloat(n1);
+    n2=parseFloat(n2);
+    soma= n1+n2;
+    return(soma);
+}
